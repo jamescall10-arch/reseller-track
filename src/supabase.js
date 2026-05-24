38c9677d89b1709df272d7aa107054084435056b
jamescall10-arch <jamescall10@gmail.com>
2026-05-24 23:36:56 +0100

    Create supabase.js

---
+++ src/supabase.js
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js'
+
+export const supabase = createClient(
+  'https://hrpzkakrvgigtxtcyjzk.supabase.co',
+  'sb_publishable_5gSsjZt69GRKBLVl-awByg_7YjJx7op'
+)
